3fcaa83705a539dbc058ab02648c749ea9b88e5b
Eduardo-Alanis-Garcia <al440184@uaeh.edu.mx>
2025-07-09 12:25:24 -0600

    Dia miercoles mitad

--- Analisis de series de tiempo en ecologia/Sesion_Martes.R
+++ Analisis de series de tiempo en ecologia/Sesion_Martes.R
@@ -65,3 +65,127 @@ multi_ews_eg2 <- multiEWS(data = pre_simTransComms[,2:7],
                           burn_in = 50,
                           threshold = 2)
 plot(multi_ews_eg2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################
+library(EWSmethods)
+library(tidyverse)
+library(patchwork)
+
+data("CODrecovery")
+
+par(mfrow = c(3, 1))  
+plot(x = CODrecovery$scenario1$time, y = CODrecovery$scenario1$biomass, type = "l", xlab = "Year", ylab = "Abundance", main = "Recovering cod population")
+plot(x = CODrecovery$scenario2$time, y = CODrecovery$scenario2$biomass, type = "l", xlab = "Year", ylab = "Abundance", main = "Recovering cod population")
+plot(x = CODrecovery$scenario3$time, y = CODrecovery$scenario3$biomass, type = "l", xlab = "Year", ylab = "Abundance", main = "Recovering cod population")
+dev.off()
+
+
+# Idea usando add
+plot(x = CODrecovery$scenario1$time, y = CODrecovery$scenario1$biomass, type = "l", xlab = "Year", ylab = "Abundance", main = "Recovering cod population")
+lines(x = CODrecovery$scenario2$time, y = CODrecovery$scenario2$biomass, col = "red")
+lines(x = CODrecovery$scenario3$time, y = CODrecovery$scenario3$biomass, col = "green")
+
+# Buena
+plot(x = CODrecovery$scenario2$time, y = CODrecovery$scenario2$biomass, col = "black",  type = "l")
+abline(v = CODrecovery$scenario2$inflection_pt |>  unique(), col = "black", lty = 2)
+lines(x = CODrecovery$scenario1$time, y = CODrecovery$scenario1$biomass, col = "red")
+lines(x = CODrecovery$scenario3$time, y = CODrecovery$scenario3$biomass, col = "green")
+abline(v = CODrecovery$scenario3$inflection_pt |>  unique(), col = "green", lty = 2)
+
+
+pre_CODrecovery <- subset(CODrecovery$scenario2,time < inflection_pt)
+pre_simTransComms <- subset(simTransComms$community1,time < inflection_pt)
+
+
+####################################
+### Ejercicios cortesia de Cesar ###
+####################################
+
+greenhouse = read.table(file = "Analisis de series de tiempo en ecologia/ENSC_2025_EWS/end_of_green_house.txt")
+dryland = read.table(file = "Analisis de series de tiempo en ecologia/ENSC_2025_EWS/dryland_ecosystem.txt")
+desertification = read.table(file = "Analisis de series de tiempo en ecologia/ENSC_2025_EWS/desertification.txt")
+
+greenhouse$time = c(1:nrow(greenhouse))
+dryland$time = c(1:nrow(dryland))
+desertification$time = c(1:nrow(desertification))
+
+# GreenHouse
+
+greenhouse = greenhouse |> 
+  dplyr::select(time,V1) |> 
+  dplyr::rename(valores=V1)
+
+greenhouse_rolling = EWSmethods::uniEWS(data = greenhouse,
+                   metrics = c("ar1","SD","skew"),
+                   method = "rolling",
+                   winsize = 50)
+plot(greenhouse_rolling,  y_lab = "Density")
+
+
+greenhouse_expanding = EWSmethods::uniEWS(data = greenhouse,
+                               metrics = c("ar1","SD","skew"),
+                               method = "expanding", 
+                               burn_in = 50,
+                               threshold = 2)
+plot(greenhouse_expanding,  y_lab = "Density")
+
+### Dryand
+
+dryland = dryland |> 
+  dplyr::select(time,V1) |> 
+  dplyr::rename(valores = V1)
+
+dryland_rolling = EWSmethods::uniEWS(data = dryland,
+                                        metrics = c("ar1","SD","skew"),
+                                        method = "rolling",
+                                        winsize = 50)
+plot(dryland_rolling,  y_lab = "Density")
+
+
+dryland_expanding = EWSmethods::uniEWS(data = dryland,
+                                          metrics = c("ar1","SD", "skew"),
+                                          method = "expanding", 
+                                          burn_in = 50,
+                                          threshold = 2)
+plot(dryland_expanding,  y_lab = "Density")
+
+
+
+
+#### desertification
+
+desertification = desertification |> 
+  dplyr::select(time,V1) |> 
+  dplyr::rename(valores = V1)
+
+desertification_rolling = EWSmethods::uniEWS(data = desertification,
+                                     metrics = c("ar1","SD","skew"),
+                                     method = "rolling",
+                                     winsize = 50)
+plot(desertification_rolling,  y_lab = "Density")
+
+
+desertification_expanding = EWSmethods::uniEWS(data = desertification,
+                                       metrics = c("ar1","SD","skew"),
+                                       method = "expanding", 
+                                       burn_in = 30,
+                                       threshold = 2)
+plot(desertification_expanding,  y_lab = "Density")
+
